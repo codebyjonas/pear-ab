c96e0a4c1d26dc7066ce365a8c39572b67dbfef8
Jonas Olaussen <jonasolaussen@gmail.com>
2019-02-01 14:28:09 +0100

    Added the three sub-pages with routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
-import Header from './components/page-elements/Header'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Orders from './pages/Orders'
+import Deliveries from './pages/Deliveries'
+import Products from './pages/Products'
 
 class App extends Component {
-
     state = {
-        title: 'Pear AB | Orders'
+        title: 'Pear AB |'
     }
 
     render() {
         return (
-            <Header title={this.state.title} />
+            <Router>
+                <Switch>
+                    <Route path='/' exact render={(props) => <Orders {...props} title={'Orders'} />} />
+                    <Route path='/deliveries' exact render={(props) => <Deliveries {...props} title={'Deliveries'} />} />
+                    <Route path='/products' exact render={(props) => <Products {...props} title={'Products'} />} />
+                </Switch>
+            </Router>
         )
     }
 }
---
+++ src/pages/Orders.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Orders = props => (
+    <div>
+        <h1>{props.title}</h1>
+    </div>
+)
+
+export default Orders
