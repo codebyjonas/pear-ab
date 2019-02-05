6c6f22bd61b490dd40e39e0c3bf3b24e3008d528
Jonas Olaussen <jonasolaussen@gmail.com>
2019-02-05 17:36:54 +0100

    added stickyheader

--- src/components/orders/OrdersHeader.js
+++ src/components/orders/OrdersHeader.js
@@ -1,12 +1,36 @@
 import React from 'react'
 
-const OrdersHeader = props => (
-    <div className='orders-header-container table-header-container'>
-        <div><span>Datum</span></div>
-        <div><span>Produkt</span></div>
-        <div><span>Till / från</span></div>
-        <div><span>Antal</span></div>
-    </div>
-    )
+
+class OrdersHeader extends React.Component {
+
+
+    handleScroll() {
+        const nav = document.getElementsByClassName('orders-header-container')[0]
+        const navTop = 147
+        console.log(navTop)
+        if (window.scrollY > navTop) {
+            nav.classList.add('sticky')
+            document.body.style.paddingTop = nav.offsetHeight+'px'
+        } else {
+            nav.classList.remove('sticky')
+            document.body.style.paddingTop = 0
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+
+    render() {
+        return (
+            <div className='orders-header-container table-header-container'>
+                <div><span>Datum</span></div>
+                <div><span>Produkt</span></div>
+                <div><span>Till / från</span></div>
+                <div><span>Antal</span></div>
+            </div>
+        )
+    }
+}
 
 export default OrdersHeader
